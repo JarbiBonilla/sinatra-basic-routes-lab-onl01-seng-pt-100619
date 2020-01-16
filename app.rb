0f3b656ed536e9aea8deb6a5dc626a95303d9f97
Jarbi Bonilla <jarbibonilla@gmail.com>
2020-01-16 06:19:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,11 @@ class App < Sinatra::Base
     "My name is Jarbi Bonilla"
   end 
   
+  get '/hometown' do 
+    "My hometown is Stafford, VA"
+  end 
   
+  get '/favorite-song' do
+    "My favorite song is Somebody Else by The 1975"
+  end 
 end
